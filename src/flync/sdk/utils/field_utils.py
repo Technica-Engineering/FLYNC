19019e04ddb0333ac8524e92cf530c7f8f501f08
Houssem Benali <hbenali@technica-engineering.net>
2026-03-03 23:01:29 +0100

    chore: remove unused code

--- src/flync/sdk/utils/field_utils.py
+++ src/flync/sdk/utils/field_utils.py
@@ -1,9 +1,4 @@
-from functools import lru_cache
-from typing import Iterable, Optional, TypeVar, get_args, get_origin
-
-from pydantic import BaseModel
-
-from flync.core.annotations import External
+from typing import Iterable, Optional, TypeVar
 
 T = TypeVar("T")
 
@@ -34,42 +29,3 @@ def get_name(
         getattr(named_object, attr_name, fallback_name)
         or type(named_object).__name__
     )
-
-
-@lru_cache(maxsize=None)
-def build_model_dependencies(model: type[BaseModel]) -> dict:
-    deps = {}
-
-    for name, field in model.model_fields.items():
-        annotation = field.annotation
-        external = get_metadata(field.metadata, External)
-
-        origin = get_origin(annotation)
-        args = get_args(annotation)
-
-        if origin is list:
-            inner = args[0]
-            if isinstance(inner, type) and issubclass(inner, BaseModel):
-                deps[name] = {
-                    "external": external,
-                    "type": f"list[{inner.__name__}]",
-                    "children": build_model_dependencies(inner),
-                }
-        if origin is dict:
-            inner = args[1]
-            if isinstance(inner, type) and issubclass(inner, BaseModel):
-                deps[name] = {
-                    "external": external,
-                    "type": f"dict[{inner.__name__}]",
-                    "children": build_model_dependencies(inner),
-                }
-        elif isinstance(annotation, type) and issubclass(
-            annotation, BaseModel
-        ):
-            deps[name] = {
-                "external": external,
-                "type": annotation.__name__,
-                "children": build_model_dependencies(annotation),
-            }
-
-    return deps
