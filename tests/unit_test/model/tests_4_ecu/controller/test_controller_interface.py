f5653ee5a8bd91b5e896141034de4f36433c1450
Yav Tomar <259582188+tomaryav@users.noreply.github.com>
2026-06-11 15:36:38 +0200

    test: controller interface name propogated

--- tests/unit_test/model/tests_4_ecu/controller/test_controller_interface.py
+++ tests/unit_test/model/tests_4_ecu/controller/test_controller_interface.py
@@ -27,6 +27,29 @@ def test_positive_controller_interface_config(
     assert isinstance(ctrl.ethernet_interfaces[0].interface_config, ControllerInterface)
 
 
+def test_interface_name_propagated_from_ethernet_interface(
+    virtual_controller_interface: VirtualControllerInterface,
+    embedded_metadata_entry,
+):
+    """The interface name is no longer stored on ControllerInterface; it must be propagated
+    from the parent EthernetInterface (implied from the folder name) onto interface_config.name."""
+    ctrl_interface = {
+        "mac_address": "aa:bb:cc:dd:ee:ff",
+        "virtual_interfaces": [virtual_controller_interface],
+    }
+    ctrl = Controller.model_validate(
+        {
+            "controller_metadata": embedded_metadata_entry,
+            "name": "controller_test",
+            "ethernet_interfaces": [{"name": "interface_test", "interface_config": ctrl_interface}],
+        }
+    )
+
+    eth = ctrl.ethernet_interfaces[0]
+    assert eth.name == "interface_test"
+    assert eth.interface_config.name == "interface_test"
+
+
 def test_negative_controller_interface_wrong_mac(
     virtual_controller_interface: VirtualControllerInterface,
     embedded_metadata_entry,
