9ce091cad1ffb049a51c5ad206720c45c0732186
Dr. Lars Völker <28217814+LarsVoelker@users.noreply.github.com>
2026-06-13 21:31:35 +0200

    fix: remove controller interface double name (#187) (#191)
    * fix: remove controller interface double name
    * fix: errors multicast tests
    * fix: tests and ecu diagrams
    * fix: add private name to host controller
    * test: controller interface name propogated
    
    Co-authored-by: tomaryav <259582188+tomaryav@users.noreply.github.com>

--- tests/unit_test/model/tests_4_ecu/controller/test_controller_interface.py
+++ tests/unit_test/model/tests_4_ecu/controller/test_controller_interface.py
@@ -13,7 +13,6 @@ def test_positive_controller_interface_config(
     embedded_metadata_entry,
 ):
     ctrl_interface = {
-        "name": "interface_test",
         "mac_address": "aa:bb:cc:dd:ee:ff",
         "virtual_interfaces": [virtual_controller_interface],
         "ptp_config": None,
@@ -28,12 +27,34 @@ def test_positive_controller_interface_config(
     assert isinstance(ctrl.ethernet_interfaces[0].interface_config, ControllerInterface)
 
 
+def test_interface_name_propagated_from_ethernet_interface(
+    virtual_controller_interface: VirtualControllerInterface,
+    embedded_metadata_entry,
+):
+    """The interface name is no longer stored on ControllerInterface; it must be propagated
+    from the parent EthernetInterface (implied from the folder name) onto interface_config.name."""
+    ctrl_interface = {
+        "mac_address": "aa:bb:cc:dd:ee:ff",
+        "virtual_interfaces": [virtual_controller_interface],
+    }
+    ctrl = Controller.model_validate(
+        {
+            "controller_metadata": embedded_metadata_entry,
+            "name": "controller_test",
+            "ethernet_interfaces": [{"name": "interface_test", "interface_config": ctrl_interface}],
+        }
+    )
+
+    eth = ctrl.ethernet_interfaces[0]
+    assert eth.name == "interface_test"
+    assert eth.interface_config.name == "interface_test"
+
+
 def test_negative_controller_interface_wrong_mac(
     virtual_controller_interface: VirtualControllerInterface,
     embedded_metadata_entry,
 ):
     ctrl_interface = {
-        "name": "interface_test",
         "mac_address": "aaa-bb-cc-dd-ee-ff",
         "virtual_interfaces": [virtual_controller_interface],
     }
@@ -52,7 +73,6 @@ def test_negative_controller_interface_missing_vifaces(
     embedded_metadata_entry,
 ):
     ctrl_interface = {
-        "name": "interface_test",
         "mac_address": "aa:bb:cc:dd:ee:ff",
     }
 
@@ -68,7 +88,6 @@ def test_negative_controller_interface_missing_vifaces(
 
 def test_negative_controller_interface_empty_vifaces(embedded_metadata_entry):
     ctrl_interface = {
-        "name": "interface_test",
         "mac_address": "aa:bb:cc:dd:ee:ff",
         "virtual_interfaces": [],
     }
